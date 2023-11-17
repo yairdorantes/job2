587c469e8bc806b4fd9a178cb8ab8304f92459fe
unknown <yair>
2023-11-17 12:52:12 -0600

    ha

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
 // export const api = "http://127.0.0.1:8000/api";
 export const api = "https://job2-production.up.railway.app/api";
 // export const apiWA = "http://localhost:3000";
-export const apiWA = "http://ec2-18-205-238-236.compute-1.amazonaws.com:80";
+export const apiWA = "http://ec2-18-205-238-236.compute-1.amazonaws.com:443";

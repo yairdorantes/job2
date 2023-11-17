5c2a0e720a0d1a3138cb92736a810350e8e9c84f
unknown <yair>
2023-11-17 16:13:53 -0600

    losl2

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
-export const api = "http://127.0.0.1:8000/api";
-// export const api = "https://job2-production.up.railway.app/api";
+// export const api = "http://127.0.0.1:8000/api";
+export const api = "https://job2-production.up.railway.app/api";
 // export const apiWA = "http://localhost:3000";
 export const apiWA = "https://job2-production-8cd3.up.railway.app";
 export const accessKey = "123";

a537426addfb107b072f07153e6e797ff5ef347f
yair <yairmasterlol@gmail.com>
2023-11-17 23:17:18 -0600

    2023-11-17-11:17:18 PM

--- src/api.js
+++ src/api.js
@@ -1,3 +1,3 @@
-export const api = "http://127.0.0.1:8000/api";
-// export const api = "https://job2-production.up.railway.app/api";
+// export const api = "http://127.0.0.1:8000/api";
+export const api = "https://job2-production.up.railway.app/api";
 export const accessKey = "123";

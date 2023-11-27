4b59d9c0723df7c0aa5975c764465edda99cd19c
unknown <yair>
2023-11-27 11:30:19 -0600

    lol

--- src/components/InvText.jsx
+++ src/components/InvText.jsx
@@ -1,5 +1,58 @@
+import { useState } from "react";
 import people from "../media/meet2.svg";
+import useStore from "../Context";
+import { useEffect } from "react";
+
 const InvText = () => {
+  const { location } = useStore();
+  const [bodyText, setBodyText] = useState({
+    company: "American Cotton",
+    end: "Departamento de Recursos Humanos.",
+    location:
+      "22 de diciembre de 2023 a las 15:00 hrs. en el salón “Jardín Buenavista”, ubicado en Buenavista, San Mateo Atenco.",
+    confirmation: "Confirma tu asistencia antes del 11 de diciembre de 2023.",
+  });
+  const scrollToMap = () => {
+    const content = document.getElementById("sec3");
+    content.scrollIntoView({
+      behavior: "smooth",
+      block: "end",
+      inline: "nearest",
+    });
+  };
+
+  useEffect(() => {
+    if (location === 4) {
+      setBodyText({
+        company: "División FX",
+        end: "Departamento de Recursos Humanos.",
+        location:
+          "16 de diciembre de 2023 a las 15:00 hrs. en el salón “Jardín”, ubicado en Av. Independencia Bo. De Guadalupe N° 1212, San Mateo Atenco",
+        confirmation:
+          "Confirma tu asistencia antes del 11 de diciembre de 2023.",
+      });
+    } else if (location === 1) {
+      setBodyText({
+        company: "American Cotton",
+        end: "Departamento de Recursos Humanos.",
+        location:
+          "22 de diciembre de 2023 a las 15:00 hrs. en el salón “Jardín Buenavista”, ubicado en Buenavista, San Mateo Atenco.",
+        confirmation:
+          "Confirma tu asistencia antes del 11 de diciembre de 2023.",
+      });
+    } else if (location === 2) {
+      setBodyText({
+        company: "American Cotton",
+        end: "Ing. Rosalía Albarrán Rodríguez",
+        location:
+          "12 de diciembre de 2023 a las 14:00 hrs. en el salón “Vicky´s”, ubicado en Ron de Hank, Prolongación, Don Catarino González Benítez, Colonia Guadalupe, 52600 Santiago Tianguistenco, Méx.",
+        confirmation:
+          "Confirma tu asistencia antes del 30 de noviembre de 2023.",
+      });
+    } else if (location === 3) {
+    }
+  }, [location]);
+
   return (
     <section className="mt-20">
       <div className="flex gap-10 flex-col justify-center items-center">
@@ -10,25 +63,23 @@ const InvText = () => {
           <p className="text-black w-lg sm:w-1/2 text-[18px] mx-auto text-center  mt-5">
             Gracias por compartir este año de éxitos y desafíos apasionantes.
             <p className="mt-2">
-              American Cotton admira y valora tu trabajo, es por eso que te
+              {bodyText.company} admira y valora tu trabajo, es por eso que te
               hacemos la cordial invitación para asistir a nuestra celebración
               de fin de año, en agradecimiento a su entera colaboración,
               dedicación y esfuerzo, el{" "}
-              <span className="font-bold">
-                12 de diciembre de 2023 a las 14:00 hrs. en el salón “Vicky´s”
+              <span className="font-bold link" onClick={scrollToMap}>
+                {bodyText.location}
               </span>
-              , ubicado en Ron de Hank, Prolongación, Don Catarino González
-              Benítez, Colonia Guadalupe, 52600 Santiago Tianguistenco, Méx.
             </p>{" "}
-            Confirma tu asistencia antes del 30 de noviembre de 2023.
+            <p>{bodyText.confirmation}</p>
             <p>
               ¡Feliz año nuevo para ti y tus seres queridos! Eres una pieza
               invaluable para el equipo American Cotton.
             </p>
             <p className="mt-6">
               <span className="font-bold">Cordialmente:</span>
-              <p>Directora de Planta</p>
-              <p>Ing. Rosalía Albarrán Rodríguez</p>
+              {location === 2 && <p>Directora de Planta</p>}
+              <p>{bodyText.end}</p>
             </p>
           </p>
         </div>
--- src/components/Sec3.jsx
+++ src/components/Sec3.jsx
@@ -1,22 +1,49 @@
+import { useEffect, useState } from "react";
+import useStore from "../Context";
+
 const Sec3 = () => {
+  const { location } = useStore();
+  const [locationMaps, setLocationMaps] = useState("");
+  console.log(location);
+  useState;
+  // useEffect(() => {
+  //   if (location === 4 || location === 1) {
+  //     setLocationMaps(
+  //       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3766.1894654567823!2d-99.5386424!3d19.274125200000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cdf5299fb9169d%3A0xdb9ebb6dcb2ebe19!2sJard%C3%ADn%20de%20Fiestas%20Buenavista!5e0!3m2!1ses-419!2smx!4v1701104039602!5m2!1ses-419!2smx"
+  //     );
+  //   } else {
+  //     setLocationMaps("");
+  //   }
+  // }, [location]);
   return (
-    <section className="mt-24   mx-auto">
-      <div className="text-center  mb-10 text-red-600 font-Paragraph font-bold text-2xl">
+    <section id="sec3" className="mt-24  mx-auto">
+      <div className="text-center  mb-8 text-red-600 font-Paragraph font-bold text-2xl">
         <div className=" font-monsterrat">Ubicación</div>
         {/* <hr className="h-10  text-black w-10 "/> */}
         <div className="border-t border-1 border-red-500 my-1 w-20 mx-auto"></div>
       </div>
       <div className="sm:w-1/2 w-lg mx-auto">
-        <iframe
-          className="w-full h-[400px] rounded-sm"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3768.390675226299!2d-99.47607004366625!3d19.178131471275425!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cdf396dfc66c9f%3A0xecd70be607df12c0!2sSal%C3%B3n%20Vicky&#39;s!5e0!3m2!1ses-419!2smx!4v1700017829348!5m2!1ses-419!2smx"
-          // width="100%"
-          // height="350"
-          // style="border:0;"
-          allowFullScreen=""
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-        ></iframe>
+        {location === 2 && (
+          <iframe
+            className="w-full h-[400px] rounded-sm"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3768.390675226299!2d-99.47607004366625!3d19.178131471275425!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cdf396dfc66c9f%3A0xecd70be607df12c0!2sSal%C3%B3n%20Vicky&#39;s!5e0!3m2!1ses-419!2smx!4v1700017829348!5m2!1ses-419!2smx"
+            // width="100%"
+            // height="350"
+            // style="border:0;"
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        )}
+        {(location === 1 || location === 4) && (
+          <iframe
+            className="w-full h-[400px] rounded-sm"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3766.1894654567823!2d-99.5386424!3d19.274125200000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cdf5299fb9169d%3A0xdb9ebb6dcb2ebe19!2sJard%C3%ADn%20de%20Fiestas%20Buenavista!5e0!3m2!1ses-419!2smx!4v1701104039602!5m2!1ses-419!2smx"
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        )}
       </div>
     </section>
   );
